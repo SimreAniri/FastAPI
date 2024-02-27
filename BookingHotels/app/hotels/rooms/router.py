a81cba0b72007634e73d7943afa324cf4c97a442
SimreAniri <49020224+SimreAniri@users.noreply.github.com>
2024-02-27 18:15:38 +0300

    Delete BookingHotels/app directory

--- BookingHotels/app/hotels/rooms/router.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-from app.hotels.router import router
-
-from app.hotels.rooms.dao import RoomsDAO
-
-@router.get("/{hotel_id}/rooms")
-async def get_rooms(hotel_id: int, date_from: date, date_to: date):
-    return await RoomsDAO.left_rooms(hotel_id, date_from, date_to)
